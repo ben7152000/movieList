cd2cb2439ad8b7d6e6555ab2c1c2a5679b028116
Ben Chou <ben7152000@gmail.com>
2021-07-04 16:06:02 +0800

    movieList

---
+++ src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Enter movie name to search..." v-model="word">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" @click="searchHandler(word)">Search</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+      word: ''
+    }
+  },
+  props: {
+    keyword: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    searchHandler (word) {
+      this.$emit('update:keyword', word)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="container py-5">
-    <div class="row">
-      <div class="col">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Enter movie name to search..." v-model="keyword">
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" @click="search">Search</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Search :keyword.sync="keyword"/>
     <div class="row">
       <MovieCard v-for="(movieList,index) in searchData.length ? searchData : movieLists" :key="index" :movieList="movieList"/>
     </div>
@@ -18,10 +9,14 @@
 
 <script>
 import MovieCard from '@/components/MovieCard'
+import Search from '@/components/Search'
+
 import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {
+    Search,
     MovieCard
   },
   data () {
@@ -40,8 +35,8 @@ export default {
         console.log(e)
       })
   },
-  methods: {
-    search () {
+  watch: {
+    keyword () {
       if (this.keyword) {
         this.searchData = this.movieLists.filter(value => {
           return value.title.toLowerCase().indexOf(this.keyword.toLowerCase()) !== -1
